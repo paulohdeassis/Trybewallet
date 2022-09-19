80d62d645abfa7d605bfb224656d9af1e433335e
Paulo Henrique <paulo.henrique.hunter@gmail.com>
2022-09-18 21:44:25 -0300

    finaliza o requisito 5

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,6 +1,6 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 // import { ERROR, LOADING, SUCCESS } from '../actions/Wallet';
-import { CURRENCY, EXPENSES, REMOVE_EXPENSE } from '../actions/Wallet';
+import { CURRENCY, EXPENSES } from '../actions/Wallet';
 
 const CURRENT_STATE = {
   currencies: [],
---
+++ src/tests/aplication.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouterAndRedux from './helpers/renderWith';
+
+import App from '../App';
+
+describe('Realiza os testes para tentar atingir 60% de cobertura da aplicação', () => {
+  test('Verifica se o a tela de login habilita o botão de salvar apenas depois de receber um email e senha corretos', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const submitBtn = screen.getByRole('button', { name: /Entrar/i });
+
+    const validPassword = '123456';
+    const invalidPassword = '12345';
+    const validMail = 'paulo.henrique.hunter@gmail.com';
+    const invalidMail = 'paulo.henrique.hunter.gmail.com';
+
+    userEvent.type(emailInput, invalidMail);
+    userEvent.type(passwordInput, invalidPassword);
+
+    expect(submitBtn).toBeDisabled();
+
+    userEvent.clear(emailInput);
+    userEvent.clear(passwordInput);
+
+    userEvent.type(emailInput, validMail);
+    userEvent.type(passwordInput, invalidPassword);
+
+    userEvent.clear(emailInput);
+    userEvent.clear(passwordInput);
+
+    userEvent.type(emailInput, validMail);
+    userEvent.type(passwordInput, validPassword);
+
+    expect(submitBtn).not.toBeDisabled();
+  });
+
+  test('testa se ao clicar no botão quando habilitado a função leva para página de gastos', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const submitBtn = screen.getByRole('button', { name: /Entrar/i });
+
+    const validPassword = '123456';
+    const validMail = 'paulo.henrique.hunter@gmail.com';
+
+    userEvent.type(emailInput, validMail);
+    userEvent.type(passwordInput, validPassword);
+    userEvent.click(submitBtn);
+
+    expect(history.location.pathname).toBe('/carteira');
+  });
+});
